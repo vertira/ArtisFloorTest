7e06cd203fc848efba449327270c40330e11a7a8
Krystian G <144816009+vertira@users.noreply.github.com>
2024-04-16 01:46:31 +0200

    loadable added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { Navigation } from "./components/Navigation.jsx";
+// import { Navigation } from "./components/Navigation.jsx";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/home.jsx";
-import { Wycena } from "./pages/wycena/wycena.jsx";
-import { Firma } from "./pages/firma/firma.jsx";
-import { Contact } from "./pages/Contact.jsx";
+// import { Wycena } from "./pages/wycena/wycena.jsx";
+// import { Firma } from "./pages/firma/firma.jsx";
+// import { Contact } from "./pages/Contact.jsx";
 import { useBackground } from "./hook/useBackground.jsx";
-import { BackgroundVideo } from "./pages/home/backgroundVideo.jsx";
+// import { BackgroundVideo } from "./pages/home/backgroundVideo.jsx";
+import loadable from "@loadable/component";
 import "./index.css";
+import { useParagAnimation } from "./pages/home/useParagAnimation.jsx";
+const Navigation = loadable(() => import("./components/Navigation.jsx"), {
+	resolveComponent: (components) => components.Navigation,
+});
+const BackgroundVideo = loadable(
+	() => import("./pages/home/backgroundVideo.jsx"),
+	{
+		resolveComponent: (components) => components.BackgroundVideo,
+	}
+);
+const Firma = loadable(() => import("./pages/firma/firma.jsx"), {
+	resolveComponent: (components) => components.Firma,
+});
+const Contact = loadable(() => import("./pages/Contact.jsx"), {
+	resolveComponent: (components) => components.Contact,
+});
+const Wycena = loadable(() => import("./pages/wycena/wycena.jsx"), {
+	resolveComponent: (components) => components.Wycena,
+});
 
 export const App = () => {
+	const animation = useParagAnimation();
 	const backgroundClass = useBackground();
 	return (
 		<div
