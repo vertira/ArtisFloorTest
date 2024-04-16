9787d3e9c202348f040100a41cabc3f88621c7ff
Krystian G <144816009+vertira@users.noreply.github.com>
2024-04-16 13:10:56 +0200

    update Helmet

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useBackground } from "./hook/useBackground.jsx";
 import loadable from "@loadable/component";
 import "./index.css";
 import { useParagAnimation } from "./pages/home/useParagAnimation.jsx";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 const Navigation = loadable(() => import("./components/Navigation.jsx"), {
 	resolveComponent: (components) => components.Navigation,
 });
@@ -38,38 +37,7 @@ export const App = () => {
 			<div className="min-h-screen flex flex-col backdrop-brightness-50 backdrop-saturate-50 backdrop-contrast-150 ">
 				<Navigation />
 				<Routes>
-					<Route
-						path="/"
-						element={
-							<HelmetProvider>
-								<Home>
-									<Helmet>
-										<title>Artis Floor - cyklinowanie</title>
-										<meta
-											name="description"
-											content="Profesjonalne usługi cyklinowania podłóg i schodów drewnianych."
-										/>
-										<meta
-											name="keywords"
-											content="cyklinowanie podłóg, cyklinowanie schodów, odnawianie parkietu, renowacja podłóg drewnianych, artis floor, cyklinowanie kalisz, schody kalisz, podłoga kalisz"
-										/>
-										<meta
-											property="og:title"
-											content="Artis Floor - cyklinowanie"
-										/>
-										<meta
-											property="og:description"
-											content="Profesjonalne usługi cyklinowania podłóg i schodów drewnianych."
-										/>
-										<meta
-											property="og:image"
-											content="https://artis-floor-test.vercel.app/assets/firma/firma.svg"
-										/>
-									</Helmet>
-								</Home>
-							</HelmetProvider>
-						}
-					/>
+					<Route path="/" element={<Home />} />
 					<Route path="/firma" element={<Firma />} />
 					<Route path="/wycena" element={<Wycena />} />
 					<Route path="/kontakt" element={<Contact />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<HelmetProvider>
+			<Router>
+				<App />
+			</Router>
+		</HelmetProvider>
 	</React.StrictMode>
 );
