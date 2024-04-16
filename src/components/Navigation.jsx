af6d4ef2ed33794ec8f25125b93216287c33231f
Krystian G <144816009+vertira@users.noreply.github.com>
2024-04-16 12:54:03 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -20,7 +20,7 @@ export const Navigation = () => {
 					<img
 						src={logo}
 						alt="logo"
-						className="ml-6 mt-6 md:ml-0 md:mt-0 h-12 w-auto md:h-16 xl:h-20  "
+						className="ml-6 mt-6 md:ml-0 md:mt-0 h-12 w-auto md:h-16 xl:h-20 aspect-auto"
 					/>
 				</Link>
 			</div>
