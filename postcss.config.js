edf98e5b5caef64e149ea2ef8f3ca1783f5d122e
Krystian G <144816009+vertira@users.noreply.github.com>
2024-04-17 02:57:48 +0200

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	content: [
+		"./src/**/*.html",
+		"./src/**/*.jsx",
+		// etc.
+	],
+
+	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+	plugins: [
+		require("tailwindcss"),
+		require("autoprefixer"),
+		...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+	],
+};
